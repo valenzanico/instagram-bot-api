a952dc3fa6780f993090c6151dee0dd9858cf99f
Nicolò Valenza <nico.vale.spazio@gmail.com>
2023-03-09 21:50:35 +0100

    fix bug to send dm

--- src/instagrambotapi/senddm.py
+++ src/instagrambotapi/senddm.py
@@ -15,13 +15,14 @@ class Senddm:
             "url":f"https://www.instagram.com/{user}",
             "wait": (5,6)
         },
-        #  {
-        #     #1-Clicca sul pulsante per seguire il ricevente
-        #     "description": "start follow user",
-        #     "command": "script",
-        #     "script": '''document.querySelector("#mount_0_0_07 > div > div > div.x9f619.x1n2onr6.x1ja2u2z > div > div > div > div.x78zum5.xdt5ytf.x10cihs4.x1t2pt76.x1n2onr6.x1ja2u2z > div.x9f619.xnz67gz.x78zum5.x168nmei.x13lgxp2.x5pf9jr.xo71vjh.x1uhb9sk.x1plvlek.xryxfnj.x1c4vz4f.x2lah0s.xdt5ytf.xqjyukv.x1qjc9v5.x1oa3qoh.x1qughib > div.xh8yej3.x1gryazu.x10o80wk.x14k21rp.x1porb0y.x17snn68.x6osk4m > section > main > div > header > section > div._ab8w._ab94._ab99._ab9f._ab9m._ab9p._abcm > div > div:nth-child(1) > button").click()''',
-        #     "wait": (5,7)
-        # }, 
+          {
+            #1-Clicca sul pulsante per seguire il ricevente
+            "description": "start follow user",
+         "command": "click",
+            "script": (By.XPATH, "/html/body/div[2]/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/section/main/div/header/section/div[3]/div/div[1]/button/div"),
+            "wait": (5,7),
+            "required": False
+         }, 
         # {
         #     #2-Apre la chat dei Direct message con il ricevente
         #     "description": "open DM chat",
@@ -50,30 +51,30 @@ class Senddm:
             "target": (By.XPATH, "/html/body/div[2]/div/div/div[1]/div/div/div/div[1]/div[1]/section/div[2]/div/div/div[2]/div/div/div[2]/button"),
             "wait": (4,9)   
         },
-        # {
-        #     #5-ritorna alla pagina utente del ricevente per smettere di seguirlo
-        #     "description": "return to user page",
-        #     "command": "get",
-        #     "url":f"https://www.instagram.com/{user}",
-        #     "wait": (5,6),
-        #     "required":False
-        # },
-        # {
-        #     #6-clicca sul pulsante di unfollow del ricevente
-        #     "description": "click on unfollow button",
-        #     "command": "script",
-        #     "script": '''document.querySelector("#react-root > section > main > div > header > section > div.Igw0E.IwRSH.eGOV_._4EzTm > div > div.Igw0E.IwRSH.eGOV_._4EzTm.soMvl > div > span > span.vBF20._1OSdk > button").click()''',
-        #     "wait": (4,7),
-        #     "required": False
-        # },
-        # {
-        #     #7-conferma di voler smettere di seguire il ricevente
-        #     "description": "click on confirm unfollow button",
-        #     "command": "script",
-        #     "script": '''document.querySelector("body > div.RnEpo.Yx5HN > div > div > div > div.mt3GC > button.aOOlW.-Cab_").click()''',
-        #     "wait": (4,7),
-        #     "required": False
-        # }
+         {
+             #5-ritorna alla pagina utente del ricevente per smettere di seguirlo
+             "description": "return to user page",
+             "command": "get",
+             "url":f"https://www.instagram.com/{user}",
+             "wait": (5,6),
+             "required":False
+         },
+        {
+            #6-clicca sul pulsante di unfollow del ricevente
+            "description": "click on unfollow button",
+            "command": "click",
+         "target": (By.XPATH, "/html/body/div[2]/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/section/main/div/header/section/div[3]/div/div[1]/button"),
+             "wait": (4,7),
+            "required": False
+         },
+         {
+             #7-conferma di voler smettere di seguire il ricevente
+             "description": "click on confirm unfollow button",
+             "command": "click",
+             "target": (By.XPATH, "/html/body/div[6]/div/div/div[2]/div/div[6]/div/div/div/div/div"),
+             "wait": (4,7),
+             "required": False
+        }
 
         ]
 
